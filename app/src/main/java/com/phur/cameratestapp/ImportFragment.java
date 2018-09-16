52309ed403696417e30232a562bf67f2a38ea8fa
phil-hur <phil.hur@gmail.com>
2018-09-16 16:35:01 -0400

    bottom nav menu only

---
+++ app/src/main/java/com/phur/cameratestapp/ImportFragment.java
@@ -0,0 +1,32 @@
+package com.phur.cameratestapp;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ImportFragment extends Fragment {
+    public static ImportFragment newInstance() {
+        ImportFragment fragment = new ImportFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_import, container, false);
+    }
+
+}
