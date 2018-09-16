21db1c1fd5026b7980397151df47c009ebfcc3e1
phil-hur <phil.hur@gmail.com>
2018-09-16 18:00:30 -0400

    added web button

--- app/src/main/java/com/phur/cameratestapp/MainActivity.java
+++ app/src/main/java/com/phur/cameratestapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.phur.cameratestapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -7,6 +9,8 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -48,4 +52,5 @@ public class MainActivity extends AppCompatActivity {
         //Used to select an item programmatically
         //bottomNavigationView.getMenu().getItem(2).setChecked(true);
     }
+
 }
--- app/src/main/java/com/phur/cameratestapp/NewFragment.java
+++ app/src/main/java/com/phur/cameratestapp/NewFragment.java
@@ -1,17 +1,20 @@
 package com.phur.cameratestapp;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class NewFragment extends Fragment {
+public class NewFragment extends Fragment implements View.OnClickListener {
     public static NewFragment newInstance() {
         NewFragment fragment = new NewFragment();
         return fragment;
@@ -25,8 +28,42 @@ public class NewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_new, container, false);
+
+        Button b = (Button) v.findViewById(R.id.button_openpage);
+        b.setOnClickListener(this);
+        return v;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_openpage:
+                String urlAsString = "http://www.udacity.com";
+                openWebPage(urlAsString);
+                break;
+        }
+    }
+
+
+    public void openWebPage (String url) {
+        // TODO (2) Use Uri.parse to parse the String into a Uri
+        Uri webpage = Uri.parse(url);
+        // TODO (3) Create an Intent with Intent.ACTION_VIEW and the webpage Uri as parameters
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        // TODO (4) Verify that this Intent can be launched and then call startActivity
+        if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
+
     }
 
 }
+
+
+
+
+
+
